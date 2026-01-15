1d72532656636b363ef1df5cb7d118d4933159a2
Josias Bueno <messias_jpb@hotmail.com>
2026-01-15 16:20:15 -0300

    feat: Integrate TallStackUI, add Phosphor Icons, and set up the initial application layout and dashboard view.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,46 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ $title ?? config('app.name') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
+    <tallstackui:script />
+    @livewireStyles
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
+<body>
+    <x-ts-layout>
+        <x-slot:header>
+            <x-ts-layout.header>
+                <x-slot:right>
+                    <x-ts-dropdown text="Hello, {{ auth()->user()->name }}!">
+                        <!-- <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <x-ts-dropdown.items text="Logout" onclick="event.preventDefault(); this.closest('form').submit();" />
+                        </form> -->
+                    </x-ts-dropdown>
+                </x-slot:right>
+            </x-ts-layout.header>
+        </x-slot:header>
+
+        <x-slot:menu>
+            <x-ts-side-bar>
+                <x-ts-side-bar.item text="Home" icon="home" :route="route('dashboard')" />
+            </x-ts-side-bar>
+        </x-slot:menu>
+        {{ $slot }}
+    </x-ts-layout>
+
+    @livewireScripts
+</body>
+
 </html>
